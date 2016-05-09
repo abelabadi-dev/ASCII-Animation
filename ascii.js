77a0b4a9fa9f6371207ffb1f9c254b8b09e45a2f
Abel <abelabadi2@gmail.com>
2016-05-09 17:38:28 -0500

    Implement font size and animation type

--- ascii.js
+++ ascii.js
@@ -2,4 +2,14 @@ window.onload = function () {
 	document.getElementById('start').onclick = function () {
 		console.log("JS working");
 	}
+	document.getElementById('animations').onchange = function () {
+		var animationType = document.getElementById('animations').value;
+		document.getElementById('txtArea').value = ANIMATIONS[animationType];
+		
+	}
+	document.getElementById('size').onchange = function(){
+		var size = document.getElementById('size').value;
+		console.log(size);
+		document.getElementById('txtArea').style.fontSize = size;
+	}
 }

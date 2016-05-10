12fa38e1c99a88e4d453c59510044ff63a46b82f
Abel <abelabadi2@gmail.com>
2016-05-09 23:42:55 -0500

    animate and menu controls

--- ascii.js
+++ ascii.js
@@ -1,18 +1,24 @@
 window.onload = function () {
-	document.getElementById('start').onclick = function () {
-		console.log("JS working");
-	}
+	var start = document.getElementById('start');
+	document.getElementById('start').disabled = true;
+	document.getElementById('stop').disabled = true;
 	document.getElementById('animations').onchange = function () {
+		document.getElementById('start').disabled = false;
 		var animationType = document.getElementById('animations').value;
 		document.getElementById('txtArea').value = ANIMATIONS[animationType];
 	}
 	document.getElementById('size').onchange = function(){
+		
 		var size = document.getElementById('size').value;
 		//console.log(size);
 		document.getElementById('txtArea').style.fontSize = size;
 	}
 	var counter=0;
 	document.getElementById('start').onclick = function () {
+		document.getElementById('animations').disabled = true;
+		document.getElementById('start').disabled = false;
+		document.getElementById('stop').disabled = false;
+
 		var drawing = document.getElementById('txtArea').value;
 		//var arry = ANIMATIONS["exercise"].split("=====\n");
 		var arry = drawing.split("=====\n");
@@ -27,6 +33,13 @@ window.onload = function () {
 			counter=0;
 		}
 		},250);
-		
+		document.getElementById('stop').onclick = function () {
+		clearInterval(timer);
+		document.getElementById('animations').disabled = false;
+		var animationType = document.getElementById('animations').value;
+		document.getElementById('txtArea').value = ANIMATIONS[animationType];
+		document.getElementById('stop').disabled = true;
 	}
+	}
+	
 }

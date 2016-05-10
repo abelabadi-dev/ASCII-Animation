9598e7b0e786d35fd72e4f8e1e9dd27e9547d589
Abel <abelabadi2@gmail.com>
2016-05-09 23:22:58 -0500

    animate

--- ascii.js
+++ ascii.js
@@ -5,16 +5,28 @@ window.onload = function () {
 	document.getElementById('animations').onchange = function () {
 		var animationType = document.getElementById('animations').value;
 		document.getElementById('txtArea').value = ANIMATIONS[animationType];
-		
 	}
 	document.getElementById('size').onchange = function(){
 		var size = document.getElementById('size').value;
 		//console.log(size);
 		document.getElementById('txtArea').style.fontSize = size;
 	}
+	var counter=0;
 	document.getElementById('start').onclick = function () {
 		var drawing = document.getElementById('txtArea').value;
+		//var arry = ANIMATIONS["exercise"].split("=====\n");
 		var arry = drawing.split("=====\n");
-		document.getElementById('txtArea').value = arry[i];
+		//console.log(arry);
+		var timer = setInterval(function () {
+			if(counter < arry.length){
+			document.getElementById('txtArea').value = arry[counter];
+			counter++;
+			//console.log(counter);
+		}
+		else{
+			counter=0;
+		}
+		},250);
+		
 	}
 }
